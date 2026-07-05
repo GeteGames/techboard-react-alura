339004cb9d7f942c569a5f453fed9eca5ae9f8fb
Paulo <pauloassistente01@gmail.com>
2026-07-04 21:03:40 -0300

    Adicionando assessao do tema

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
-import { FormularioDeEvento } from "./componentes/FormularioDeEvento"
+import { FormularioDeEvento } from "./componentes/FormularioDeEvento";
+import { Tema } from "./componentes/Tema";
 
 // no react, componentes são FUNÇÕES
 
 function App() {
+  const temas = [
+    { id: 1, nome: "front-end" },
+    { id: 2, nome: "back-end" },
+    { id: 3, nome: "devops" },
+    { id: 4, nome: "inteligência articial" },
+    { id: 5, nome: "data science" },
+    { id: 6, nome: "cloud" },
+  ];
+
   return (
     <main>
       <header>
@@ -13,6 +23,24 @@ function App() {
         <img src="/banner.png" alt="" />
       </section>
       <FormularioDeEvento />
+      <section>
+        <Tema tema={temas[0]}></Tema>
+      </section>
+            <section>
+        <Tema tema={temas[1]}></Tema>
+      </section>
+            <section>
+        <Tema tema={temas[2]}></Tema>
+      </section>
+            <section>
+        <Tema tema={temas[3]}></Tema>
+      </section>
+            <section>
+        <Tema tema={temas[4]}></Tema>
+              <section>
+        <Tema tema={temas[5]}></Tema>
+      </section>
+      </section>
     </main>
   );
 }
--- src/componentes/ListaSuspensa/index.jsx
+++ src/componentes/ListaSuspensa/index.jsx
@@ -1,14 +1,15 @@
 import "./lista-formulario.estilo.css";
 
-export function ListaSuspensaFormulario({ children }) {
-
+export function ListaSuspensaFormulario() {
   return (
     <select name="temaEvento" className="form-lista-suspensa">
-      {children.map((evento) => {
-        return <option id={evento.id} value={evento.value}>
-          {evento.nome}
-        </option>;
-      })}
+      <option></option>
     </select>
   );
 }
+
+// {children.map((evento) => {
+//   return <option id={evento.id} value={evento.value}>
+//     {evento.nome}
+//   </option>;
+// })}
